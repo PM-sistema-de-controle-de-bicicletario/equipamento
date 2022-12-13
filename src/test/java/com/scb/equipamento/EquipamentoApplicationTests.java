9a937b5a0c100eb26fdc7c757f8eaea55f3c1910
Victor Hugo Marques Rosa Leite <hvictorleite@gmail.com>
2022-12-13 20:08:08 -0300

    first commit

---
+++ src/test/java/com/scb/equipamento/EquipamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scb.equipamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
